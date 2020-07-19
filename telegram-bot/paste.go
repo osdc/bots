baf1992e8fbcd36e63249778d371c181705a76c6
daemon1024 <barun1024@gmail.com>
2020-07-19 20:48:56 +0530

    send tg document data to pastebin

--- telegram-bot/paste.go
+++ telegram-bot/paste.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -10,9 +11,22 @@ import (
 )
 
 func paste(ID int64, message *tbot.Message) {
-	data := message.ReplyToMessage.Text
-	log.Println(data)
-	resp, err := http.Post("https://paste.rs/", "application/octet-stream", strings.NewReader(data))
+	var resp *http.Response
+	var err error
+	if message.ReplyToMessage.Text != "" {
+		data := message.ReplyToMessage.Text
+		log.Println("text", data)
+		resp, err = http.Post("https://paste.rs/", "application/octet-stream", strings.NewReader(data))
+	} else {
+		data := message.ReplyToMessage.Document
+		filelink, errf := bot.GetFileDirectURL(data.FileID)
+		if errf != nil {
+			log.Fatalln(errf)
+		}
+		filebytes, _ := http.Get(filelink)
+		file, _ := ioutil.ReadAll(filebytes.Body)
+		resp, err = http.Post("https://paste.rs/", "application/octet-stream", bytes.NewBuffer(file))
+	}
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -21,5 +35,5 @@ func paste(ID int64, message *tbot.Message) {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	log.Println("hello", string(body))
+	log.Println(string(body))
 }
