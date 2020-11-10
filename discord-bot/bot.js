88b076a20a97a136f22a4b2fe3303eebf97a9915
reapedjuggler <52465652+reapedjuggler@users.noreply.github.com>
2020-11-10 16:46:50 +0530

    Added a default message for the invalid commands (#55)
    * Update bot.js
    
    Added a default message for the commands which aren't valid
    
    * Added a default message for the invalid commands #53
    
    Issue #53 ,Bot was not showing any response for unknown triggers. Fixed it by adding a default message whenever a invalid command is triggered.
    
    * Bot not responding to unknown Commands #53
    
    Issue #53, Bot was not showing any response for unknown triggers. Fixed it by adding a default message whenever a invalid command is triggered.
    
    * Update bot.js
    
    * Removed unwanted return statement

--- discord-bot/bot.js
+++ discord-bot/bot.js
@@ -9,7 +9,6 @@ bot.on('ready', () => {
     console.log('The bot is online!!!!')
 })
 
-
 //Greeting message for a New user!!!
 
 bot.on('guildMemberAdd',member => {
@@ -27,7 +26,7 @@ bot.on('message',message => {
             .setTitle('Here are the list of commands ')
             .addFields(
                 {name : '!help', value: "To view the list of commands " ,inline:false},
-                {name : '!website', value: "Visit our Website" ,inline:false},
+                {name : '!website', value: "Visit our Website" ,inline:false}, 
                 {name : '!facebook', value: "Follow us on Facebook " ,inline:false},
                 {name : '!twitter', value: "Check us out on Twitter " ,inline:false},
                 {name : '!github', value: "Visit our Github Repository " ,inline:false},
@@ -36,7 +35,6 @@ bot.on('message',message => {
                 {name : '!irc', value: "Find us on IRC :) " ,inline:false},
                 {name : '!blog', value: "Get the link of OSDC blog" ,inline:false},
                 {name : '!instagram', value: "Follow us on instagram :) " ,inline:false},
-
             )
             message.channel.send(commandEmbedded)
         }
@@ -45,63 +43,68 @@ bot.on('message',message => {
 //xkcd comic command
 //It first randomly generate a number between 100 to 2000 and then sends the http request with the generated number to fetch comic 
 
-
 bot.on('message',message => {
 if(message.content === '!xkcd')
-{ 
+   { 
     let comicNo = Math.floor(Math.random() * (2000 - 100 + 1) + 100)
     
     axios.get(`http://xkcd.com/${comicNo}/info.0.json`).then(resp => {
     
         message.channel.send(resp.data.img)
     });
-    
-    }
+   }
     
 })
 
 //Social media commands
 
 bot.on('message',message => { 
+
    if(message.content === '!website')
    {    
     const website = new Discord.MessageEmbed().setTitle('Visit our Website').setURL('https://osdc.netlify.app/')
     message.channel.send(website)
    }
-   if(message.content === '!twitter')
+   else if(message.content === '!twitter')
    {
         const twitter =  new Discord.MessageEmbed().setTitle('Check us out on Twitter').setURL('https://twitter.com/osdcjiit')
         message.channel.send(twitter)
    }
-   if(message.content === '!facebook')
+   else  if(message.content === '!facebook')
    {
         const facebook = new Discord.MessageEmbed().setTitle('Follow us on Facebook').setURL('https://www.facebook.com/JIIT-OSDC-169171359799320/')
         message.channel.send(facebook)
    }
-    if(message.content === '!github')
+   else  if(message.content === '!github')
     {
-        const github = new Discord.MessageEmbed().setTitle('Take a look at our cool projects').setURL('https://github.com/osdc')
+        const github = new Discord.MessageEmbed().setTitle(' Take a look at our cool projects ').setURL('https://github.com/osdc')
         message.channel.send(github)
     }
-    if(message.content === '!telegram')
+    else  if(message.content === '!telegram')
     {
         const telegram = new Discord.MessageEmbed().setTitle(' Join our Telegram Channel').setURL('https://t.me/jiitosdc')
         message.channel.send(telegram)
     } 
-    if(message.content === '!irc')
+    else  if(message.content === '!irc')
     {
         const irc = new Discord.MessageEmbed().setTitle(' Join us on IRC server of Freenode at #jiit-lug').setURL('https://github.com/osdc/community-committee/wiki/IRC')
         message.channel.send(irc)
     } 
-    if(message.content === '!blog')
+    else  if(message.content === '!blog')
     {
         const blog = new Discord.MessageEmbed().setTitle('Blogs written by the folks at the Open Source Developers Community').setURL('https://osdcblog.netlify.com/')
         message.channel.send(blog)
     } 
     
-    if(message.content === '!instagram')
+    else  if(message.content === '!instagram')
     {
         message.channel.send('https://tenor.com/view/dont-do-that-avengers-black-panther-we-dont-do-that-here-gif-12042935')
+    }
+
+    else if(message.content[0] === '!')
+    {
+        const sorry = new Discord.MessageEmbed().setTitle('Command Not Found , try !help for reference')    
+        message.channel.send(sorry)
     }
         
 })
